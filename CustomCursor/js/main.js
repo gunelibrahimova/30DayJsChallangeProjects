c95a5d05e29f919a813d6e8f79a7346e07aa75d5
gunelibrahimova <75317328+gunelibrahimova@users.noreply.github.com>
2023-03-30 22:33:01 +0400

    Custom cursor

---
+++ CustomCursor/js/main.js
@@ -0,0 +1,19 @@
+const root = document.querySelector('html')
+
+const cursor = document.createElement('div')
+cursor.classList.add('cursor')
+root.appendChild(cursor)
+
+const follower = document.createElement('div')
+follower.classList.add('cursor', 'cursor__follower')
+root.appendChild(follower)
+
+
+root.addEventListener('mousemove', (e) => {
+  setPosition(follower, e)
+  setPosition(cursor, e)
+})
+
+function setPosition(element, e) {
+  element.style.transform = `translate3d(${e.clientX}px, ${e.clientY}px, 0)`
+}
